f50b9f8db949f509de4a0f4c8dd798c288d31d2d
Kseniia Machnieva <164485727+Ksenia-m4@users.noreply.github.com>
2024-07-29 07:48:56 +0200

    The second and third tasks were done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,45 @@
+// В HTML есть <iframe> с видео для Vimeo плеера. Напиши скрипт который будет сохранять текущее время воспроизведения видео в локальное хранилище и, при перезагрузке страницы, продолжать воспроизводить видео с этого времени.
 
+//1. Ознакомься с документацией библиотеки Vimeo плеера.
+//2. Добавь библиотеку как зависимость проекта через npm.
+//3. Инициализируй плеер в файле скрипта как это описано в секции pre-existing player, но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
+//4. Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
+//5. Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка "videoplayer-current-time".
+//6. При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
+//7. Добавь в проект библиотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
+
+import trottle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+// on(event: string, callback: function): void = метод ON
+player.on('timeupdate', trottle(onTimeupdate, 1000));
+
+function onTimeupdate(evt) {
+  let timeToSave = evt.seconds;
+  localStorage.setItem(LOCAL_STORAGE_KEY, timeToSave);
+}
+
+function getCurrentTime() {
+  return localStorage.getItem(LOCAL_STORAGE_KEY);
+}
+
+player
+  .setCurrentTime(getCurrentTime())
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+//  Выполняй это задание в файлах 03-feedback.html и 03-feedback.js. Разбей его на несколько подзадач:
 
+// 1. Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
+// 2. При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
+// 3. При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
+// 4. Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
+
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(OnFormInput, 500)); // вешаю событие на импут
+formEl.addEventListener('submit', onFormSubmit); // вешаю событие на сабмит
+
+const STORAGE_KEY = 'feedback-msg';
+const formFeedbackData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+fillForm(); // заполняю форму из local storage
+
+function OnFormInput(evt) {
+  formFeedbackData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formFeedbackData));
+}
+
+function fillForm() {
+  const parsedMassage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!parsedMassage) {
+    return;
+  }
+
+  const { email: savedEmail, message: savedMessage } = parsedMassage;
+  const { email, message } = formEl; // забираю с формы элементы
+
+  email.value = savedEmail ?? '';
+  message.value = savedMessage ?? '';
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const form = evt.currentTarget;
+
+  const { email, message } = form.elements;
+
+  if (!(email.value === '' || message.value === '')) {
+    console.log(formFeedbackData);
+
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    return;
+  } else {
+    alert('Все поля должны быть заполнены!');
+    return;
+  }
+}
